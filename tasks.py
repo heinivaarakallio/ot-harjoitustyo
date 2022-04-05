b851faac4845ac9b77d8e4432a23425100329777
heinivaarakallio <heini.vaarakallio@helsinki.fi>
2022-04-06 01:22:02 +0300

    modified tasks.py file

--- tasks.py
+++ tasks.py
@@ -1,17 +1,25 @@
 from invoke import task
 
 @task
-def foo(ctx):
-    print("bar")
-
-@task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
+
+@task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
