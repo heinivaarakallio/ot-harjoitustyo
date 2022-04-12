8b7a633f06c6fb4f70949592cf53014f7dad588e
heinivaarakallio <heini.vaarakallio@helsinki.fi>
2022-04-12 19:31:20 +0300

    wrote code to entities

---
+++ src/entities/spaceship.py
@@ -0,0 +1,14 @@
+import pygame
+import os
+
+dirname = os.path.dirname(__file__)
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self, x=0, y=0):
+        super().__init__()
+        self.image = pygame.image.load(
+            os.path.join(dirname, "..", "assets", "spaceship.png")
+        )
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
